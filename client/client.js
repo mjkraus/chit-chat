9f44fed46e2a437467d0ac89a9a8281c7f9e7721
Molly Kraus <mollyjkraus@gmail.com>
2016-02-20 17:16:50 -0500

    added comments to files

--- client/client.js
+++ client/client.js
@@ -1,21 +1,28 @@
 /**
 * Templates
 */
+
+// the client and server logic is divided by .isClient and .isServer. It is often organized by 
+// dividing the logic into different folders.
+// Need to explore the below if statement more to know what it is doing. 
+// I think it is sorting the messages from the DB based on the timestamp. This is how they are displayed
+// in our view
+
 if (Meteor.isClient) {
     Template.messages.helpers({
         messages: function() {
             return Messages.find({}, { sort: { time: -1}});
         }
     });
-
+// the function below is for the input tag and submit button. 
 Template.input.events = {
 	
    'click #submit' : function (event) {
-    // 13 is the enter key event
       var name = document.getElementById('name');
       // var name = 'Anonymous';
       var message = document.getElementById('message');
 
+//If the value of the message is not empty then insert the values into DB
       if (message.value != '') {
         Messages.insert({
           name: name.value,
@@ -23,6 +30,7 @@ Template.input.events = {
           time: Date.now(),
         });
 
+//resets the values in the input fields
         document.getElementById('message').value = '';
         message.value = '';
         document.getElementById('name').value = '';
--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 /**
 * Create new collection if not present.
 */
+// this is calling our DB and creating a "table" called messages
 Messages = new Meteor.Collection('messages');
